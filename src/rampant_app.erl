146aa4268df3aa8d6759c9d441ed8b1c4fcdeb91
Benjamin Bastian <benjamin.bastian@gmail.com>
2016-03-23 20:15:49 -0700

    Add app outline

---
+++ src/rampant_app.erl
@@ -0,0 +1,16 @@
+-module(rampant_app).
+
+-behaviour(application).
+
+-export([
+    start/2,
+    stop/1
+]).
+
+
+start(_StartType, _StartArgs) ->
+    rampant_sup:start_link().
+
+
+stop(_State) ->
+    ok.
